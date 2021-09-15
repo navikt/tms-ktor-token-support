2b3dd2a9ca448f6028fc9b63b8ed589d2e80058c
Christopher Santana <christopher.santana@nav.no>
2021-09-15 12:23:36 +0200

    Åpner for at bruker av tokenx-validation modulen kan overstyre hvilket claim fnr skal hentes fra.

--- token-support-tokenx-validation/src/main/kotlin/no/nav/tms/token/support/tokenx/validation/user/TokenXUserFactory.kt
+++ token-support-tokenx-validation/src/main/kotlin/no/nav/tms/token/support/tokenx/validation/user/TokenXUserFactory.kt
@@ -10,17 +10,17 @@ object TokenXUserFactory {
 
     private val IDENT_CLAIM = "pid"
 
-    fun createTokenXUser(call: ApplicationCall): TokenXUser {
+    fun createTokenXUser(call: ApplicationCall, identClaim: String = IDENT_CLAIM): TokenXUser {
         val principal = call.principal<TokenXPrincipal>()
                 ?: throw Exception("Principal har ikke blitt satt for authentication context.")
 
-        return createIdportenUser(principal)
+        return createIdportenUser(principal, identClaim)
     }
 
-    private fun createIdportenUser(principal: TokenXPrincipal): TokenXUser {
+    private fun createIdportenUser(principal: TokenXPrincipal, identClaim: String): TokenXUser {
         val token = principal.decodedJWT
 
-        val ident: String = token.getClaim(IDENT_CLAIM).asString()
+        val ident: String = token.getClaim(identClaim).asString()
         val loginLevel = extractLoginLevel(token)
         val expirationTime =
             getTokenExpirationLocalDateTime(
