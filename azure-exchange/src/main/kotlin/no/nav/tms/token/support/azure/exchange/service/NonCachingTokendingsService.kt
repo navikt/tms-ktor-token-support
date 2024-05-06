ffa5885d6d62d18f2a3a557e7afe931488196345
rannveigskjerve <55136985+rannveigskjerve@users.noreply.github.com>
2024-05-06 13:34:10 +0200

    Tokenexchange errorhandling (#31)
    *  tokendings errorhandling
    * azureexchange errorhandling
    
    Closes #29

--- azure-exchange/src/main/kotlin/no/nav/tms/token/support/azure/exchange/service/NonCachingTokendingsService.kt
+++ azure-exchange/src/main/kotlin/no/nav/tms/token/support/azure/exchange/service/NonCachingTokendingsService.kt
@@ -8,43 +8,50 @@ import no.nav.tms.token.support.azure.exchange.consumer.AzureConsumer
 
 
 class NonCachingAzureService internal constructor(
-        private val azureConsumer: AzureConsumer,
-        issuer: String,
-        clientId: String,
-        privateJwk: String
-): AzureService {
+    private val azureConsumer: AzureConsumer,
+    issuer: String,
+    clientId: String,
+    privateJwk: String
+) : AzureService {
 
     private val clientAssertionService = ClientAssertionService(privateJwk, clientId, issuer)
 
-    override suspend fun getAccessToken(targetApp: String): String {
+    override suspend fun getAccessToken(targetApp: String): String = try {
         val jwt = clientAssertionService.createClientAssertion()
 
-        return azureConsumer.fetchToken(jwt, targetApp).accessToken
+        azureConsumer.fetchToken(jwt, targetApp).accessToken
+    } catch (throwable: Throwable) {
+        throw AzureExchangeException(throwable, targetApp)
     }
+
+
 }
 
 class CachingAzureService internal constructor(
-        private val azureConsumer: AzureConsumer,
-        issuer: String,
-        clientId: String,
-        privateJwk: String,
-        maxCacheEntries: Long,
-        cacheExpiryMarginSeconds: Int,
-): AzureService {
+    private val azureConsumer: AzureConsumer,
+    issuer: String,
+    clientId: String,
+    privateJwk: String,
+    maxCacheEntries: Long,
+    cacheExpiryMarginSeconds: Int,
+) : AzureService {
 
     private val cache = CacheBuilder.buildCache(maxCacheEntries, cacheExpiryMarginSeconds)
 
     private val clientAssertionService = ClientAssertionService(privateJwk, clientId, issuer)
 
 
-    override suspend fun getAccessToken(targetApp: String): String {
+    override suspend fun getAccessToken(targetApp: String): String =
+        try {
+            cache.get(targetApp) {
+                runBlocking {
+                    performTokenExchange(targetApp)
+                }
+            }.accessToken
+        } catch (throwable: Throwable) {
+            throw AzureExchangeException(throwable, targetApp)
+        }
 
-        return cache.get(targetApp) {
-            runBlocking {
-                performTokenExchange(targetApp)
-            }
-        }.accessToken
-    }
 
     private suspend fun performTokenExchange(targetApp: String): AccessTokenEntry {
         val jwt = clientAssertionService.createClientAssertion()
@@ -54,3 +61,16 @@ class CachingAzureService internal constructor(
         return AccessTokenEntry.fromResponse(response)
     }
 }
+
+class AzureExchangeException(val originalThrowable: Throwable, targetApp: String) :
+    Exception() {
+
+    val stackTraceSummary =
+        originalThrowable.stackTrace.firstOrNull()?.let { stacktraceElement ->
+            """    Azureexchange feiler for $targetApp 
+                   Origin: ${stacktraceElement.fileName ?: "---"} ${stacktraceElement.methodName ?: "----"} linenumber:${stacktraceElement.lineNumber}
+                   Message: "${originalThrowable::class.simpleName} ${originalThrowable.message?.let { ":$it" }}"
+                """.trimIndent()
+        } ?: "${originalThrowable::class.simpleName} ${originalThrowable.message?.let { ":$it" }}"
+}
+
